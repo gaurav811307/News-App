102b1bce52a65b75905bcbd5a3bf2b6560a03d9e
Gyanendra Gaurav <gyanendragaurav32@gmail.com>
2024-11-12 21:04:57 +0530

    "Added API"

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "c10e7ea618434cf2957f47f6666ba750"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
